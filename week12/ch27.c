cc687c8ff56864e2548c89b8da4dd25f5826f83b
postaandras <91436854+postaandras@users.noreply.github.com>
2022-05-02 22:18:12 +0200

    Add files via upload

---
+++ week12/ch27.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void func(char* p, int x){
+    printf("p is \"%s\" and x is %i\n",p,x);
+}
+
+int main(){
+    printf("Hello world!\n");
+   
+    char* a = "Hello";
+    char* b = "World!"; 
+    printf("%s %s\n",a,b);
+
+    printf(s1);
+
+    func("asd",1);
+    func("asd2",21);
+    
+    return 0;
+}
